2a13f78dd4133545bfe81ca8a697c3bad5918e93
Siam Islam <siamislam1970@gmail.com>
2025-05-13 11:34:02 +0600

    basic arithmetic operators

---
+++ math.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    //aarithmetic operators = + - * / % ++ --
+
+    int x = 2 ;
+    int y = 3;
+    // int z = 0 ;
+    int z = 0 ;
+
+    // z = x + y ;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
+
+    // x++;
+    // x--;
+
+    // x+=3
+    // x-=3
+
+    // x*=2
+    // x/=2
+    
+    printf("%d", z);
+
+
+
+
+
+    return 0 ;
+}
